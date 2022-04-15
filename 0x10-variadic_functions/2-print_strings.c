6d4de26115ab7ecf9f6baf5a809759d2004f7fb4
UnknownTitan <ayitahs@yahoo.com>
2022-04-14 22:50:35 -0700

    Print String followed by a new line

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,5 +1,5 @@
 /*
- *File: 1-print_numbers.c
+ *File: 2-print_strings.c
  *Auth: Stephen Nii Armah Ayitiah
  */
 
@@ -8,21 +8,30 @@
 #include <stdarg.h>
 
 /**
- * print_numbers - Prints numbers, followed by a new line.
- * @separator: The string to be printed between numbers.
- * @n: The number of integers passed to the function.
- * @...: A variable number of numbers to be printed.
+ * print_strings - Prints strings, followed by a new line.
+ * @separator: The string to be printed between strings.
+ * @n: The number of strings passed to the function.
+ * @...: A variable number of strings to be printed.
+ *
+ * Description: If separator is NULL, it is not printed.
+ *              If one of the strings if NULL, (nil) is printed instead.
  */
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list nums;
+	va_list strings;
+	char *str;
 	unsigned int index;
 
-	va_start(nums, n);
+	va_start(strings, n);
 
 	for (index = 0; index < n; index++)
 	{
-		printf("%d", va_arg(nums, int));
+		str = va_arg(strings, char *);
+
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
 
 		if (index != (n - 1) && separator != NULL)
 			printf("%s", separator);
@@ -30,5 +39,5 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	printf("\n");
 
-	va_end(nums);
+	va_end(strings);
 }
